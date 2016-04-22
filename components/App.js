2c54e6042fe16d992f15a37d6a17f5a6e78fe62d
flowstyleliving <mys628@gmail.com>
2016-04-21 20:35:33 -0700

    finished part 5 of tutorial

--- components/App.js
+++ components/App.js
@@ -1,1 +1,15 @@
+import React, { Component } from 'react';
+import TextInput from './TextInput';
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>App Component</h1>
+        <TextInput/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ components/TextDisplay.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class TextDisplay extends Component {
+  handleClick() {
+    this.props.deleteLetter();
+  }
+  render() {
+    return(
+      <div>
+        <div>
+          I am displaying text from my parent: {this.props.text}
+        </div>
+        <button onClick={this.handleClick.bind(this)}>delete one letter</button>
+      </div>
+    )
+  }
+}
+
+export default TextDisplay;
